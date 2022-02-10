92c5367d86a50f633687b9f85af1ff264842b3f3
Md Abdulla Heal Masud <heal.masud@gmail.com>
2022-02-10 20:30:08 +0200

    added todo events

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,49 @@
 import React from "react";
 
-function Form() {
+const Form = ({ inputText, setInputText, todos, setTodos, setStatus }) => {
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      { text: inputText, completed: false, id: Math.random() * 1000 },
+    ]);
+    setInputText("");
+  };
+
+  const statusHandler = (e) => {
+    setStatus(e.target.value);
+  };
+
   return (
-    <form>
-      <input type="text" className="todo-input" />
-      <button className="todo-button" type="submit">
-        <i className="fas fa-plus-square"></i>
-      </button>
-      <div className="select">
-        <select name="todos" className="filter-todo">
-          <option value="all">All</option>
-          <option value="completed">Completed</option>
-          <option value="uncompleted">Uncompleted</option>
-        </select>
-      </div>
-    </form>
+    <>
+      <form>
+        <input
+          type="text"
+          className="todo-input"
+          value={inputText}
+          onChange={inputTextHandler}
+        />
+        <button
+          type="submit"
+          className="todo-button"
+          onClick={submitTodoHandler}
+        >
+          <i className="fas fa-plus-square" />
+        </button>
+        <div className="select" onChange={statusHandler}>
+          <select className="filter-todo" name="todos">
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="uncompleted">Uncompleted</option>
+          </select>
+        </div>
+      </form>
+    </>
   );
-}
+};
 
 export default Form;
